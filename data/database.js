69cf79ca3a8bed47a242a9eb0b6c365c4e28f9f9
richardlyonheart <brendenlyon907@gmail.com>
2025-03-23 18:39:14 -0400

    fixed db

--- data/database.js
+++ data/database.js
@@ -10,21 +10,20 @@ const initDb = (callback) => {
         console.log('Db is already initialized');
         return callback(null, database);
     }
-    MongoClient.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    MongoClient.connect(process.env.MONGODB_URL)
     .then((client) => {
-        database = client.db("project2");
-        console.log("Database initialized!");
+        database = client;
         callback(null, database);
     })
     .catch((err) => {
-        console.error("Failed to initialize database", err);
         callback(err);
     });
+
 };
 
 const getDatabase = () => {
     if (!database) {
-        throw Error('Database not initialized');
+       throw Error('Database not initialized')
     }
     return database;
 };
