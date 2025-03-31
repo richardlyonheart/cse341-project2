9c7f4f752c142d052487c85c32924bfe04d30f44
richardlyonheart <brendenlyon907@gmail.com>
2025-03-31 17:24:29 -0400

    updated try catch

--- controllers/project2.js
+++ controllers/project2.js
@@ -2,62 +2,80 @@ const { response } = require('express');
 const mongodb = require('../data/database');
 const ObjectId = require("mongodb").ObjectId;
 
-const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db('project2').collection('project2').find();
-    result.toArray().then((project2) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(project2);
-    });
+const getSingle = async (req, res) => {
+  try {
+    const CarId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db('project2').collection('project2').findOne({ _id: CarId });
+
+    if (!result) {
+      res.status(404).json({ message: "Car not found" });
+      return;
+    }
+
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(result);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message || "An error occurred while retrieving the car" });
+  }
 };
 
-const getSingle = async (req, res) => {
-    const CarId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db('project2').collection("project2").find({ _id: CarId });
-    result.toArray().then((cars) => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).json(cars[0]);
-    });
-  };
-
 const createCar = async (req, res) => {
-  const car ={
-    brand: req.body.brand,
-    type: req.body.type,
-  };
-  const response = await mongodb.getDatabase().db("project2").collection("project2").insertOne({car});
-  if (response.acknowledged){
-    res.status(200).send();
-  } else {
-    res.status(500).json(response.error || "there has been an error creating car")
+  try {
+    const car = {
+      brand: req.body.brand,
+      type: req.body.type,
+    };
+
+    const response = await mongodb.getDatabase().db('project2').collection('project2').insertOne(car);
+
+    if (!response.acknowledged) {
+      throw new Error("Failed to create the car");
+    }
+
+    res.status(201).json({ id: response.insertedId, message: "Car created successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message || "An error occurred while creating the car" });
   }
 };
 
 const updateCar = async (req, res) => {
-  const CarId = new ObjectId(req.params.id);
-  const car = {
-    brand: req.body.brand,
-    type: req.body.type,
-  };
-  const response = await mongodb.getDatabase().db("project2").collection("project2").replaceOne({ _id: CarId}, car);
+  try {
+    const CarId = new ObjectId(req.params.id);
+    const car = {
+      brand: req.body.brand,
+      type: req.body.type,
+    };
 
-  if (response.modifiedCount > 0) {
-    res.status(200).send();
-  } else { 
-    res.status(500).json(response.error || "there has been an error updating car")
+    const response = await mongodb.getDatabase().db('project2').collection('project2').replaceOne({ _id: CarId }, car);
+
+    if (response.modifiedCount === 0) {
+      throw new Error("Car not found or no updates were made");
+    }
+
+    res.status(200).json({ message: "Car updated successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message || "An error occurred while updating the car" });
   }
 };
 
-const deleteCar = async (req, res)=> {
-  const CarId = new ObjectId(req.params.id);
-  const response = await mongodb.getDatabase().db("project2").collection("project2").deleteOne({ _id: CarId }, true);
+const deleteCar = async (req, res) => {
+  try {
+    const CarId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db('project2').collection('project2').deleteOne({ _id: CarId });
 
-  if (response.deletedCount >0) {
-    res.status(200).send();
-  } else {
-    res.status(500).json(response.erros || "could not delete car")
+    if (response.deletedCount === 0) {
+      throw new Error("Car not found");
+    }
+
+    res.status(200).json({ message: "Car deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message || "An error occurred while deleting the car" });
   }
 };
-
 
 module.exports = {
     getAll,
