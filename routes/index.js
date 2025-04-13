cd5bb48c01dabb521e1a5e60659292fabc8bfc22
richardlyonheart <brendenlyon907@gmail.com>
2025-04-12 23:13:03 -0400

    fixed render

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const passport = require('passport');
+const { isAuthenticated } = require('../middleware/auth');
 
 router.get('/', (req, res) => {
     res.send('Hello World');
@@ -8,5 +10,16 @@ router.use('/project2', require('./project2'));
 router.use('/houses', require('./houses'));
 
 router.use('/', require('./swagger'));
+router.get('/login', passport.authenticate('github'), (req, res) => { });
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/?message=You have been Logged out successfully');
+  });
+});
+
 
 module.exports = router;
