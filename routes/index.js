1cef17edb75beb9c11ac5a35c901d6d9759092e8
richardlyonheart <brendenlyon907@gmail.com>
2025-04-13 17:50:05 -0400

    progress

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const passport = require('passport');
 const { isAuthenticated } = require('../middleware/auth');
 
-router.use('/project2', require('./project2'));
-router.use('/houses', require('./houses'));
 
 router.use('/', require('./swagger'));
+router.use('/houses', isAuthenticated, require('/houses'));
+router.use('/project2', isAuthenticated, require('/project2'));
 router.get('/login', passport.authenticate('github'), (req, res) => { });
 
 router.get('/logout', (req, res, next) => {
