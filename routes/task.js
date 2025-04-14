b9ca7017e989527cf6a321b12d8c50c7b2bd2706
richardlyonheart <brendenlyon907@gmail.com>
2025-04-14 19:52:54 -0400

    fixed

--- routes/task.js
+++ routes/task.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const tasksController = require('../controllers/tasks');
+const tasksController = require('../controllers/task');
 const { validateTask } = require('../middlewares/validation');
 
 const { isAuthenticated } = require('../middleware/auth');
