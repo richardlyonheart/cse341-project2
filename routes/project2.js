5b088222b34e360b406cd3c6e778159d2be9b4e4
richardlyonheart <brendenlyon907@gmail.com>
2025-04-14 20:31:58 -0400

    try witout auth

--- routes/project2.js
+++ routes/project2.js
@@ -4,14 +4,14 @@ const {isAuthenticated} = require('../middleware/auth');
 
 const project2Controller= require('../controllers/project2');
 
-router.get('/', isAuthenticated, project2Controller.getAll);
+router.get('/',  project2Controller.getAll);
 
-router.get('/:id', isAuthenticated, project2Controller.getSingle);
+router.get('/:id',  project2Controller.getSingle);
 
-router.post('/', isAuthenticated, project2Controller.createCar);
+router.post('/',  project2Controller.createCar);
 
-router.put('/:id', isAuthenticated, project2Controller.updateCar);
+router.put('/:id',  project2Controller.updateCar);
 
-router.delete('/:id',isAuthenticated, project2Controller.deleteCar);
+router.delete('/:id', project2Controller.deleteCar);
 
 module.exports = router;
